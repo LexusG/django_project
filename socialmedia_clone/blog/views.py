22412f5a0fcadf9c92f5aecd09ab7259d0ecb68c
LexusG <33048777+LexusG@users.noreply.github.com>
2024-02-06 09:06:49 -0500

    Using hmtl and css templates in my django project

--- socialmedia_clone/blog/views.py
+++ socialmedia_clone/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Porkman',
+        'title':  'Blog 1',
+        'content': 'Ass is phat',
+        'date_posted': 'August 10, 2034'
+    },
+    
+    {
+        'author': 'Thoran',
+        'title':  'Blog 2',
+        'content': 'Lighting of a God',
+        'date_posted': 'June 10, 1287'
+    }
+]
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 # This function handles the traffic from the blog's homepage 
 
 def about(request):
